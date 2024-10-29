14879666b7a96892df062b33ab6e90df7addc484
zySail <15038858+zysail@user.noreply.gitee.com>
2024-10-29 12:14:26 +0800

    detect function call

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <fstream>
 #include <regex>
-#include <sstream>
-#include <string>
 #include <vector>
+//#include <sstream>
+//#include <string>
 
 using namespace std;
 
@@ -11,9 +11,8 @@ std::ifstream test;
 vector<string> functions;
 
 regex singleLineComment(R"(//.*?$)");
-//regex funcDefPattern("(?:int|float|double|void|char)\\s*([a-zA-Z_]\\w*)\\s*\\([^)]*\\)\\s*\\{");
-regex funcDefPattern("(?:int|float|double|void|char)\\s*((?!main)\\b[a-zA-Z_]\\w*)\\s*\\([^)]*\\)\\s*\\{");
-//regex funcCallPattern(R"(\b([a-zA-Z_]\w*)\s*\([^)]*\)\s*;)");
+regex funcDefPattern(R"((?:int|float|double|void|char)\s*((?!main)\b[a-zA-Z_]\w*)\s*\([^)]*\)\s*\{)");
+regex funcCallPattern(R"(([a-zA-Z_]\w*)\s*\([^)]*\)\s*;)");
 
 string removeComments(const string &s) {
     return regex_replace(s, singleLineComment, "");
@@ -28,6 +27,15 @@ void detectFuncDef(const string &s){
     return;
 }
 
+void detectFuncCall(const string &s){
+    smatch matchs;
+    if (regex_search(s, matchs, funcCallPattern)) {
+        cout << matchs[1] << endl;
+        //functions.push_back(matchs[1]);
+    }
+    return;   
+}
+
 int main(){
     test.open("D://Programming//Algorithms//NJUPT_AlgorithmsLab//problem6//test.c", std::ios::in);
     if(!test.is_open()){
@@ -37,11 +45,11 @@ int main(){
     std::string s;
     while(getline(test,s)){
     	s = removeComments(s); 
-        detectFuncDef(s);
+        detectFuncCall(s);
     }
-    for (const auto &funcName : functions) {
-        cout << funcName << endl;
-    }
+    // for (const auto &funcName : functions) {
+    //     cout << funcName << endl;
+    // }
     test.close();
     return 0;
 }
