296e0046b4289273fe12b462e30fb5fb46e4f644
zySail <15038858+zysail@user.noreply.gitee.com>
2024-10-29 11:37:35 +0800

    detect function definition

--- main.cpp
+++ main.cpp
@@ -3,25 +3,30 @@
 #include <regex>
 #include <sstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 std::ifstream test;
+vector<string> functions;
 
-string removeComments(const string &code) {
-    regex singleLineComment(R"(//.*?$)");
-    return regex_replace(code, singleLineComment, "");
+regex singleLineComment(R"(//.*?$)");
+//regex funcDefPattern("(?:int|float|double|void|char)\\s*([a-zA-Z_]\\w*)\\s*\\([^)]*\\)\\s*\\{");
+regex funcDefPattern("(?:int|float|double|void|char)\\s*((?!main)\\b[a-zA-Z_]\\w*)\\s*\\([^)]*\\)\\s*\\{");
+//regex funcCallPattern(R"(\b([a-zA-Z_]\w*)\s*\([^)]*\)\s*;)");
+
+string removeComments(const string &s) {
+    return regex_replace(s, singleLineComment, "");
 }
 
-void readline(){
-    std::string s;
-    while(getline(test,s)){
-    	s = removeComments(s); 
-        std::cout << s << std::endl;
+void detectFuncDef(const string &s){
+    smatch matchs;
+    if (regex_search(s, matchs, funcDefPattern)) {
+        //cout << matchs[1] << endl;
+        functions.push_back(matchs[1]);
     }
     return;
 }
-
 
 int main(){
     test.open("D://Programming//Algorithms//NJUPT_AlgorithmsLab//problem6//test.c", std::ios::in);
@@ -29,7 +34,14 @@ int main(){
         std::cout << "can not open test file" << std::endl;
         return 0;
     }
-    readline();
+    std::string s;
+    while(getline(test,s)){
+    	s = removeComments(s); 
+        detectFuncDef(s);
+    }
+    for (const auto &funcName : functions) {
+        cout << funcName << endl;
+    }
     test.close();
     return 0;
 }
