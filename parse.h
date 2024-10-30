c8a33c68b9379dee3e40821d945e0eecfa37a46f
zySail <15038858+zysail@user.noreply.gitee.com>
2024-10-30 11:59:40 +0800

    delete unused variable

--- parse.h
+++ parse.h
@@ -5,10 +5,6 @@ extern std::vector<std::string> functionNames;
 extern std::vector<std::vector<std::string>> calledFunctionNames;
 //extern std::map<std::string, int> Name2Num;
 
-extern std::regex singleLineComment;
-extern std::regex funcDefPattern;
-extern std::regex funcCallPattern;
-
 void removecomment(std::string src, std::string dst);
 // std::vector<std::string> parseFuncDef(std::string fileName);
 // std::vector<std::vector<std::string>> parseFuncCall(std::string fileName);
