f69ae062f9fabb1a9abb0574ad393bbfe5f06aac
zySail <15038858+zysail@user.noreply.gitee.com>
2024-11-01 12:09:09 +0800

    add DEBUG mode

--- main.cpp
+++ main.cpp
@@ -2,25 +2,19 @@
 #include <fstream>
 #include <vector>
 #include <regex>
-//#include <sstream>
-//#include <string>
 #include "parse.h"
 #include "graph.h"
 
 
 int main(){
-    //std::string testFile = "D://Programming//Algorithms//NJUPT_AlgorithmsLab//problem6//test.c";
-    //std::string new_testFile = "D://Programming//Algorithms//NJUPT_AlgorithmsLab//problem6//newtest.c";
     std::string testFile = "test.c";
     std::string new_testFile = "newtest.c";
-    // removecomment(testFile, new_testFile)
+    removecomment(testFile, new_testFile);
 
     parseFuncDef(new_testFile);
     parseFuncCall(new_testFile);
-    printRelationship();
     
     createGraph();
-    printGraph();
     findRecFunc();
     setMaxCallTimes();
     analyzeRec();
