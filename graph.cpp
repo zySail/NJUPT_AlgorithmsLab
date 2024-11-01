bfacee28acda9be7eb7445236a6144d9472d8ffc
zySail <15038858+zysail@user.noreply.gitee.com>
2024-11-01 12:05:03 +0800

    change from adj matrix to adj list

--- graph.cpp
+++ graph.cpp
@@ -2,41 +2,50 @@
 #include "graph.h"
 #include "parse.h"
 
-int size; // matirx size
-std::vector<std::vector<int>> matrix; // adj matrix
-std::vector<int> recFuncNum; // store recursive function numbers
+int nNodes; 
+std::vector<std::vector<int>> adjList; // adj list
+std::vector<int> recFuncList; // store recursive function numbers
 std::vector<int> MaxCallTimes; // store a function max call time
 std::vector<int> maxDepth; // record maxdepth a function can arrive 
 std::vector<int> callTimes; // record a function call time
 
+void printGraph(void);
+
 void createGraph(void){
-    size = functionNames.size();
-    matrix.resize(size, std::vector<int>(size, 0));
-    for(int i = 0; i < functionNames.size(); i++){
+    nNodes = functionNames.size();
+    adjList.resize(nNodes);
+    for(int i = 0; i < nNodes; i++){
         for(const std::string &funName : calledFunctionNames[i]){
-            int j = getFuncNum(funName);
-            matrix[i][j] = 1;
+            adjList[i].push_back(getFuncNum(funName));
         }
     }
+#ifdef DEBUG
+    printGraph();
+#else
+#endif
+    return;
 }
 
 void printGraph(void){
-    for(const auto &s : functionNames){
-        std::cout << s << "  ";
+    std::cout << "grapt adj list:" << std::endl;
+    for(int i = 0; i < nNodes; i++){
+        std::cout << i << ": ";
+        for(const int &n : adjList[i])
+            std::cout << n << " ";
+        std::cout << std::endl;
     }
-    std::cout << std::endl;
-    for(const auto &row : matrix){
-        for(const auto &element : row){ 
-            std::cout << element << "\t";
-        }
+    for(int i = 0; i < nNodes; i++){
+        std::cout << functionNames[i] << ": ";
+        for(const int &n : adjList[i])
+            std::cout << functionNames[n] << " ";
         std::cout << std::endl;
     }
     std::cout << std::endl;
 }
 
 void dfs(int v, int visit[]){
-    for(int i = 1; i < size; i++){
-        if(matrix[v][i] == 1 && visit[i] == 0){
+    for(const int &i : adjList[v]){
+        if(visit[i] == 0){
             visit[i] = 1;
             dfs(i, visit);
         }
@@ -44,42 +53,48 @@ void dfs(int v, int visit[]){
 }
 
 bool isRec(int v){
-    if(matrix[v][v] == 1)
-        return true;
-    int visit[size] = {0};
+    int visit[nNodes] = {0};
     dfs(v, visit);
-    // std::cout << v << ": ";
-    // for(int i = 1; i < size; i++) {
-    //     std::cout << visit[i] << " ";
-    // }
-    // std::cout << std::endl;
+
+#ifdef DEBUG
+    std::cout << "function "<< v << " visit[]" << ": ";
+    for(int i = 1; i < nNodes; i++) {
+        std::cout << visit[i] << " ";
+    }
+    std::cout << std::endl;
+#else
+#endif
+
     if(visit[v] == 1)
         return true;
     return false;
 }
 
 void findRecFunc(void){
-    for(int i = 1; i < size; i++){
+    for(int i = 1; i < nNodes; i++){
         if(isRec(i))
-            recFuncNum.push_back(i);
+            recFuncList.push_back(i);
     }
     std::cout << "Recursive Functions:" << std::endl;
-    for(const int &i : recFuncNum){
+    for(const int &i : recFuncList){
         std::cout << functionNames[i] << std::endl;
     }
     std::cout << std::endl;
 }
 
 void setMaxCallTimes(void){
-    // MaxCallTimes.resize(size, 0);
-    // for(int i = 1; i < size; i++){
-    //         std::cout << "set " << functionNames[i] << " max call times: ";
-    //         std::cin >> MaxCallTimes[i];
-    // }
+    MaxCallTimes.resize(nNodes, 0);
+#ifdef DEBUG
     MaxCallTimes = {0,4,3,1,5};
-    std::cout << "max call times: ";
-    for(int i = 1; i < size; i++){
-        std::cout << MaxCallTimes[i] << " ";
+#else
+    for(int i = 1; i < nNodes; i++){
+            std::cout << "set " << functionNames[i] << " max call times: ";
+            std::cin >> MaxCallTimes[i];
+    }
+#endif
+    std::cout << "max call times you set: ";
+    for(int i = 1; i < nNodes; i++){
+        std::cout << functionNames[i] << "(" << MaxCallTimes[i] << ") ";
     }
     std::cout << std::endl;
     std::cout << std::endl;
@@ -91,10 +106,10 @@ void dfs_depth(int v, int depth){
     if(callTimes[v] < MaxCallTimes[v])
         callTimes[v]++;
     
-    for(int i = 1; i < size; i++){
-        if((matrix[v][i] == 1) && (callTimes[i] + 1 <= MaxCallTimes[i])){
+    for(const int &i : adjList[v]){
+        if(callTimes[i] + 1 <= MaxCallTimes[i]){
             dfs_depth(i, depth+1);
-        }
+        }                                                                           
     }    
 }
 
@@ -104,16 +119,14 @@ void recordFuncCall(int start){ // record func call Times and max depth from fun
 }
 
 void analyzeRec(void){
-    maxDepth.resize(size, 0);
-    callTimes.resize(size, 0);
-    for(int i = 1; i < size; i++){
-        if(matrix[0][i] == 1){
-            recordFuncCall(i);
-        }
+    maxDepth.resize(nNodes, 0);
+    callTimes.resize(nNodes, 0);
+    for(const int i : adjList[0]){
+        recordFuncCall(i);
     }
     // printf rec funcs' call times and max depth
     std::cout << "Function Call Statistics:" << std::endl;
-    for(const int i : recFuncNum){
+    for(const int i : recFuncList){
         std::cout << functionNames[i] << " Called " << callTimes[i] << " times, Max Depth: " << maxDepth[i] << std::endl;
     }
 }
--- graph.h
+++ graph.h
@@ -3,7 +3,6 @@
 extern std::vector<std::vector<int>> matrix;
 
 void createGraph(void);
-void printGraph(void);
 void findRecFunc(void);
 void setMaxCallTimes(void);
 void analyzeRec(void);
