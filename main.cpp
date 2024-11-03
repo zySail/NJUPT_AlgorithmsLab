ed06b4da1eb447d63e5969717e380d630f19d7ab
zySail <15038858+zysail@user.noreply.gitee.com>
2024-11-03 16:33:21 +0800

    create dir for test files

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 #include "graph.h"
 
 int main(){
-    std::string testFile = "test.c";
-    std::string new_testFile = "newtest.c";
+    std::string testFile = "testfile\\test.c";
+    std::string new_testFile = "testfile\\newtest.c";
     removecomment(testFile, new_testFile);
 
     parseFuncDef(new_testFile);
