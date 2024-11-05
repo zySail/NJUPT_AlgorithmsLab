e8ac17b5c7be945c37245a32181bcae5ca83b4fd
zySail <15038858+zysail@user.noreply.gitee.com>
2024-11-05 21:01:01 +0800

    init mytest.c

--- testfile/mytest.c
+++ testfile/mytest.c
@@ -1,29 +1,23 @@
 // mytest.c test more complex condition
 
-int funcA(); // function declare
-
 int main() {
     funcA();
-    printf("hello world"); // not defined function
     return 0;
 }
 
-
-void funcA() // "{" in next line
-{
-    std::cout << "In funcA" << std::endl;
+void funcA() {
     funcB();  
-    funcC();   
+    funcC();  
 }
+
 void funcB() {
-    std::cout << "In funcB" << std::endl;
     funcA();  
 }
+
 void funcC() {
-    std::cout << "In funcC" << std::endl;
     funcD();  
 }
+
 void funcD() {
-    std::cout << "In funcD" << std::endl;
     funcD();  
 }
