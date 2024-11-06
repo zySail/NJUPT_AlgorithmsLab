0df92c34653255f4dfd04c6e48219ef669f0d35f
zySail <15038858+zysail@user.noreply.gitee.com>
2024-11-06 10:30:01 +0800

    add file

---
+++ testfile/newmytest.c
@@ -0,0 +1,30 @@
+
+
+void funcA() {
+    funcB();  
+    funcC();  
+}
+
+int main() {
+    funcA();
+    printf("hello\n");
+    funcE();
+    return 0;
+}
+
+void funcB() 
+{
+    funcA();  
+}
+
+void funcC() {
+    funcD();  
+}
+
+void funcD() {
+    funcD();  
+}
+
+void funcE(){
+    funcE();
+}
